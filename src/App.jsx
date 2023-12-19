3cfff616127916c96eeaa33524ac77c1c462f7ab
miulerbm <miulerbm00@gmail.com>
2023-12-19 16:25:05 -0500

    Intro page y configuracion de tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { useState } from "react";
+import React from "react";
+import Intro from "./components/Intro";
+import Timeline from "./components/Timeline";
+import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="App">
+        <Intro></Intro>
+        <Portfolio></Portfolio>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+        Miuler Blas
+      </h1>
+      <p className="text-base md:text-xl mb-3 font-medium">
+        Mechatronics Engineer & Software Developer
+      </p>
+      <p className="text-sm max-w-xl mb-6 font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet tempore
+        provident labore expedita quae debitis suscipit, recusandae dolorum
+        quasi perferendis optio, doloribus sequi culpa inventore placeat, sint
+        ipsa. Necessitatibus, voluptatem. Puedes ir a{" "}
+        <a
+          href="www.linkedin.com/in/miuler-blas-marquina"
+          target="_blank"
+          className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
+          rel="noreferrer noopener"
+        >
+          Linkedin
+        </a>{" "}
+        para saber más de mí.
+      </p>
+    </div>
+  );
+}
+
+export default Intro;

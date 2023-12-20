59f2af0d79bf165023f118b531e0043fedcaf973
miulerbm <miulerbm00@gmail.com>
2023-12-20 18:08:36 -0500

    updating vite deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/miulerbm.github.io/",
   plugins: [react()],
+  base: "https://miulerbm.github.io/miulerbm",
 });

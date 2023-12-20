0f387239d4cc01c70ffd33277301ab844553c91b
miulerbm <miulerbm00@gmail.com>
2023-12-20 18:29:53 -0500

    structure changes

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -4,37 +4,37 @@
 export default [
   {
     title: "React JS Calculator",
-    imgUrl: "src/assets/react-calculator.jpg",
+    imgUrl: "src/public/assets/react-calculator.jpg",
     stack: ["React", "JavaScript"],
     link: "https://github.com/miulerbm/react-calculator",
   },
   {
     title: "Database Manager App",
-    imgUrl: "src/assets/database_app.jpeg",
+    imgUrl: "../public/assets/database_app.jpeg",
     stack: ["C#", "MySQL", "Apache Server"],
     link: "https://github.com/miulerbm/DatabaseSQLMusicAppWF",
   },
   {
     title: "Fullstack Portfolio",
-    imgUrl: "src/assets/basic-portfolio.png",
+    imgUrl: "../public/assets/basic-portfolio.png",
     stack: ["JavaScript", "HTML", "CSS"],
     link: "https://github.com/miulerbm/basic-old-portfolio",
   },
   {
     title: "Salary Predictor",
-    imgUrl: "src/assets/ml_app.jpg",
+    imgUrl: "public/assets/ml_app.jpg",
     stack: ["Python", "Sci-kit", "Streamlit"],
     link: "https://github.com/miulerbm/mlWebApp",
   },
   {
     title: "IBM ML Labs",
-    imgUrl: "src/assets/ibm-ml.jpg",
+    imgUrl: "public/assets/ibm-ml.jpg",
     stack: ["Python", "Sci-kit", "Machine Learning"],
     link: "https://github.com/miulerbm/IBM_MachineLearning",
   },
   {
     title: "Sales Analytics",
-    imgUrl: "src/assets/analyst_proj.png",
+    imgUrl: "public/assets/analyst_proj.png",
     stack: ["SQL", "PowerBI", "DAX"],
     link: "https://github.com/miulerbm/SalesReportPBIX",
   },

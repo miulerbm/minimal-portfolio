8fa0a58c48e561343ef9035286c9bf9ae9f24215
miulerbm <miulerbm00@gmail.com>
2023-12-20 09:00:32 -0500

    Modo oscuro, titulos de secciones

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@
 function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 p-10">
-      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
         Miuler Blas
       </h1>
       <p className="text-base md:text-xl mb-3 font-medium">
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -2,13 +2,15 @@
 // import React from "react";
 import timeline from "../data/timeline";
 import TimelineItem from "./TimelineItem";
+import Title from "./Title";
 
 function Timeline() {
   // Here we will iterate through the TimelineItems, like in portfolio.
   return (
     // El padding p-20 es la clave para que no se malogre el timeline
-    <div className="flex flex-col md:flex-row justify-center my-20 px-16">
-      <div className=" w-full md:w-9/12">
+    <div className="flex flex-col md:flex-row justify-center my-20 px-20">
+      <div className=" w-full md:w-7/12">
+        <Title>Timeline</Title>
         {timeline.map((item) => (
           <TimelineItem
             year={item.year}

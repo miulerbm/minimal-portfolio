968ff914cbc55a6726650195672ec171dcb55ddc
miulerbm <miulerbm00@gmail.com>
2023-12-20 21:00:54 -0500

    timeline updates

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -8,17 +8,17 @@ export default [
   },
   {
     year: "2023",
-    title: "AI Researcher",
+    title: "Drone Trainer",
     duration: "(Aug 2023 - Today)",
     details:
-      "Implemented a reinforcement learning agent for controlling a quadrotor's height in SIMULINK. By using the DDPG and TD3 algorithms I was able to \"teach\" my virtual drone in to fly by himself. Currently teaching him to perform better... (I guess it's a him, he didn't tell me something different)",
+      "Implemented a reinforcement learning agent for controlling a quadrotor's height in SIMULINK. By using the DDPG and TD3 algorithms I was able to \"teach\" my virtual drone to fly by trial and error. Currently teaching him to perform better... (I guess it's a him, he didn't tell me something different)",
   },
   {
     year: "2020",
     title: "Computer Vision Programmer",
     duration: "4 months",
     details:
-      "During the 2019 fall Introduction to Artificial Intelligence Class, I learned how to build neural networks for prediction and classification purposes. It was my first approach to Python and using software for data manipulation and analysis. I made a YOLO algorithm detector for identifying and labelling the components in PCBs.",
+      "During the 2019 Introduction to Artificial Intelligence Class, I learned how to build neural networks for prediction and classification purposes. It was my first approach to Python and using software for data manipulation and analysis. I made a YOLO algorithm detector for identifying and labelling components in PCBs.",
   },
   {
     year: "2018",
@@ -29,7 +29,7 @@ export default [
   },
   {
     year: "2017",
-    title: "First Line of Code",
+    title: "Hello World!",
     duration: "the beginning",
     details:
       "My first class in programming during my studies in mechatronics engineering. I built the basic foundations and learned that programming is not about writing code but about solving problems and using code as a tool for that. I made a C++ pong game, nothing fancy, but it was fun :).",

6207422e8d1c3f44a6a9a5a6f6a7cc1fe2a22489
miulerbm <miulerbm00@gmail.com>
2023-12-20 19:05:32 -0500

    assets config

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -4,19 +4,19 @@
 export default [
   {
     title: "React JS Calculator",
-    imgUrl: "src/public/assets/react-calculator.jpg",
+    imgUrl: "public/assets/analyst_proj.png",
     stack: ["React", "JavaScript"],
     link: "https://github.com/miulerbm/react-calculator",
   },
   {
     title: "Database Manager App",
-    imgUrl: "../public/assets/database_app.jpeg",
+    imgUrl: "public/assets/database_app.jpeg",
     stack: ["C#", "MySQL", "Apache Server"],
     link: "https://github.com/miulerbm/DatabaseSQLMusicAppWF",
   },
   {
     title: "Fullstack Portfolio",
-    imgUrl: "../public/assets/basic-portfolio.png",
+    imgUrl: "public/assets/basic-portfolio.png",
     stack: ["JavaScript", "HTML", "CSS"],
     link: "https://github.com/miulerbm/basic-old-portfolio",
   },

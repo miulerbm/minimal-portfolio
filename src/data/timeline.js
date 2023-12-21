ed9d0e711cfefa2916300f4473e6ad3f28413155
miulerbm <miulerbm00@gmail.com>
2023-12-20 21:23:04 -0500

    Bio updates

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -4,18 +4,18 @@ export default [
     title: "Fullstack Developer",
     duration: "(Nov 2023 - Today)",
     details:
-      "Embarked on the journey of software development. Support coding a backoffice management application utilizing Next JS v14. Building Lambda functions in Java for programming event-driven backend actions. Deployed these functions in a cloud environment, leveraging AWS and Docker technologies. All these coding tasks within the Ubuntu distribution.",
+      "Embarked on the journey of software development. Support coding a backoffice management application utilizing Next JS v14. Building Lambda functions in Java for implementing CRUD operations in a DynamoDB database. Deployed these functions in a cloud environment, leveraging AWS and Docker technologies. All these coding tasks within the Ubuntu distribution.",
   },
   {
     year: "2023",
-    title: "Drone Trainer",
+    title: "Quadcopter Trainer",
     duration: "(Aug 2023 - Today)",
     details:
       "Implemented a reinforcement learning agent for controlling a quadrotor's height in SIMULINK. By using the DDPG and TD3 algorithms I was able to \"teach\" my virtual drone to fly by trial and error. Currently teaching him to perform better... (I guess it's a him, he didn't tell me something different)",
   },
   {
     year: "2020",
-    title: "Computer Vision Programmer",
+    title: "Artificial Intelligence Programmer",
     duration: "4 months",
     details:
       "During the 2019 Introduction to Artificial Intelligence Class, I learned how to build neural networks for prediction and classification purposes. It was my first approach to Python and using software for data manipulation and analysis. I made a YOLO algorithm detector for identifying and labelling components in PCBs.",
